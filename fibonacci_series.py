30f70986d51a7ea8747d5c21c5921ee688f2bfdc
Kent Patrick Ilao <kentpatrickilao@gmail.com>
2025-02-13 10:53:05 +0800

    Add files via upload

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -1,8 +1,8 @@
 n_terms = int(input("Enter the number of terms: "))
 
-a = 0
-b = 1
+firstnum = 0
+secondnum = 1
 
 for i in range(n_terms):
-    print(a, end=" ")
-    a, b = b, a + b
+    print(firstnum, end=" ")
+    firstnum, secondnum = secondnum, firstnum + secondnum
